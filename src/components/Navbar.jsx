aba8bd988ec20bfb2381b3aa0f2698281e19baa2
Fredrik S <fredrik.spannar@gmail.com>
2023-06-28 22:58:00 +0200

    Added active page in navbar with a tailwind custom class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation  } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faHeart, faPalette } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
+    // get current url to set which link should be active
+    const currentURL = useLocation().pathname;
 
     return (
         <>
@@ -10,9 +12,9 @@ export default function Navbar() {
                 <h1>React-Pokelist</h1>
 
                 <ul className="flex items-start ml-12 space-x-6 mt-2">
-                    <li><Link to="/"><FontAwesomeIcon icon={faHouse} /> Start</Link></li>
-                    <li><Link to="/favorites"><FontAwesomeIcon icon={faHeart} /> My Favorites</Link></li>
-                    <li><Link to="/settings"><FontAwesomeIcon icon={faPalette} /> Settings</Link></li>
+                    <li><Link to="/" className={currentURL.length == 1 ? "navlink-active" : ""}><FontAwesomeIcon icon={faHouse} /> Start</Link></li>
+                    <li><Link to="/favorites" className={currentURL.includes("favorites") ? "navlink-active" : ""}><FontAwesomeIcon icon={faHeart} /> My Favorites</Link></li>
+                    <li><Link to="/settings" className={currentURL.includes("settings") ? "navlink-active" : ""}><FontAwesomeIcon icon={faPalette} /> Settings</Link></li>
                 </ul>
 
             </nav>
